1f144241af0b7174c25ca57bdc79f69368c16925
Steven B <steven.barrow@patronusenergy.com>
2026-07-28 13:30:03 -0500

    test

--- exosphere.py
+++ exosphere.py
@@ -11,7 +11,7 @@ This app retrieves the space pictures of you latest birthday
 """)
 
 
-birthday = st.date_input('Input your birthday', min_value=datetime.date(1960-01-01), max_value="today")
+birthday = st.date_input('Input your birthday', min_value=datetime.date(1960-0o1-0o1), max_value="today")
 st.write('Your current selected birthday is', birthday)
 
 
