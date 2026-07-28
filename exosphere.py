85bea9a680f6397e7bf429286b272b11f42e9357
Steven B <steven.barrow@patronusenergy.com>
2026-07-28 13:56:22 -0500

    test

--- exosphere.py
+++ exosphere.py
@@ -1,21 +1,36 @@
 import streamlit as st
-#import requests 
-#from datetime import datetime 
+import requests 
+from datetime import date
 
+
+api_key = "mrusuysu3MxPRwSRFUi3JN3ezdTml4qbhUdVVJTU"
 
 st.title('exosphere')
+st.markdown("This app retrieves the space picture from your latest birthday")
 
+birthday = st.date_input('Input your birthday', min_value=date(1960, 1, 1), max_value=date.today())
 
-st.markdown("""
-This app retrieves the space pictures of you latest birthday
-""")
+def get_picture(d):
+    resp = requests.get(
+        "https://api.nasa.gov/planetary/apod",
+        params={"date": d.isoformat(), "api_key": api_key, "thumbs": True},
+        timeout=10,
+    )
+    resp.raise_for_status()
+    return resp.json()
 
+# most recent anniversary of the birthday
+today = date.today()
+target = birthday.replace(year=today.year)
+if target > today:
+    target = birthday.replace(year=today.year - 1)
 
-birthday = st.date_input('Input your birthday')
-st.write('Your current selected birthday is', birthday)
+st.write('Showing the picture from', target)
 
-
-
+data = get_picture(target)
+st.subheader(data["title"])
+st.image(data["url"])
+st.write(data["explanation"])
 
 
 
